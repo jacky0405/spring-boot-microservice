b73451f4e32f3112446f4534d8c7b2d3bb2b25ef
jacky <jacky0405@ymail.com>
2024-09-17 17:03:53 +0800

    Add product service

---
+++ product-service/src/test/java/com/jacky/microservice/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,44 @@
+package com.jacky.microservice.product_service;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ProductServiceApplicationTests {
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	@Test
+	void createProductTest() {
+		String requestBody = """
+				{
+				    "name": "iphone",
+				    "description": "haha",
+				    "price": 20000
+				}
+			""";
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201);
+
+	}
+
+}
