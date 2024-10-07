98dee10bf0ea3a265d07bb2b67fb14ff6cff8a4c
jacky <jacky0405@ymail.com>
2024-10-07 21:56:51 +0800

    Add inventory service

---
+++ inventory-service/src/main/java/com/jacky/microservice/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.jacky.microservice.inventory_service.repository;
+
+import com.jacky.microservice.inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
